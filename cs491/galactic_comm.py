436200a5671b6d84a149e0cf9376bb60158d6749
wintercresss <135405194+wintercresss@users.noreply.github.com>
2025-02-19 17:23:39 -0600

    floyd warshall esque problem

---
+++ cs491/galactic_comm.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+grid = [[0 for _ in range(n)] for _ in range(n)]
+
+for r in range(n):
+    col = list(map(int, input().split()))
+    grid[r] = col.copy()
+
+k = int(input())
+
+for _ in range(k):
+    u, v, cost = map(int, input().split())
+    # edge u to v, with cost c
+    u -= 1
+    v -= 1
+
+    total = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            grid[i][j] = min(grid[i][j], grid[i][u] + cost + grid[v][j], grid[i][v] + cost + grid[u][j])
+            grid[j][i] = grid[i][j]
+            total += grid[i][j]
+    
+    print(total, end=" ")
