e94a45404fc1a1ddf0d253454fc5ea6978a2d8af
wintercresss <135405194+wintercresss@users.noreply.github.com>
2025-02-21 06:22:41 -0600

    mst problem

---
+++ cs491/MST.py
@@ -0,0 +1,40 @@
+import heapq
+
+n, m = map(int, input().split())
+d = [[] for _ in range(n)]
+
+def find_mst(d, startnode, n):
+    heap = [(0, startnode, -1)] # -1 = prev
+    visited = set()
+    res = []
+    total = 0
+
+    while len(visited) < n:
+        currcost, currnode, prevnode = heapq.heappop(heap)
+        if currnode in visited:
+            continue
+        
+        total += currcost
+        visited.add(currnode)
+        if prevnode != -1:
+            res.append((prevnode+1, currnode+1))
+        for nextnode, cost in d[currnode]:
+            heapq.heappush(heap, (cost, nextnode, currnode))
+
+    res.sort(key=lambda x:x[1])
+    return res, total
+
+
+for _ in range(m):
+    u, v, cost = map(int, input().split())
+    u -= 1
+    v -= 1
+    d[u].append((v, cost))
+    d[v].append((u, cost))
+
+answer, totalcost = find_mst(d, 0, n)
+
+print(totalcost)
+for u, v in answer:
+    print(str(u) + " " + str(v))
+
