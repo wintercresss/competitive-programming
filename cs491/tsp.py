fe0e4921647ca20e346a3b0002f75d5c9a51f657
wintercresss <135405194+wintercresss@users.noreply.github.com>
2025-04-25 20:59:14 -0500

    fixed TSP bitmask DP question

--- cs491/tsp.py
+++ cs491/tsp.py
@@ -1,27 +1,38 @@
-from collections import defaultdict
-import heapq
-
-graph = defaultdict(list)
-
+from functools import cache
 n, m = map(int, input().split())
+# n = nodes, m = edges
+
+cost = [[float('inf') for _ in range(n)] for _ in range(n)]
+
 for _ in range(m):
-    u, v, d = map(int, input().split())
-    graph[u].append((v, d))
-    graph[v].append((u, d))
+    u, v, dist = map(int, input().split())
+    u -= 1
+    v -= 1
+    cost[u][v] = dist
+    cost[v][u] = dist
 
-print(graph)
 
-heap = [(0, 1)] # dist, node
-visited = set()
+for i in range(n):
+    cost[i][i] = 0
 
-while heap:
-    currcost, currnode = heapq.heappop(heap)
-    if currnode in visited:
-        continue
-    visited.add(currnode)
-    print(visited, currcost)
-    if len(visited) == n:
-        break
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j])
 
-    for nextnode, nextcost in graph[currnode]:
-        heapq.heappush(heap, (currcost + nextcost, nextnode))
+# goal: visit all nodes
+# mask represents nodes that are taken?
+@cache
+def dp(mask, last):
+    if mask == (1 << n) - 1:
+        return 0
+    
+    best = float('inf')
+    for j in range(n):
+        if mask & (1 << j):
+            continue
+        best = min(best, cost[last][j] + dp(mask | (1 << j), j))
+    return best
+
+ans = min(dp(1 << i, i) for i in range(n))
+print(ans)
